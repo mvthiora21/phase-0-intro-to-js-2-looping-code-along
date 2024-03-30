1fe4569611915d7942aaab4b8d0890be23b25a1b
victormuthiora <victormuthiora617@gmail.com>
2024-03-30 13:00:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, eventName) {
+    let cardsArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      cardsArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cardsArray;
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Example usage
+  let messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(messages);
+  
+  countDown(5);
+  
